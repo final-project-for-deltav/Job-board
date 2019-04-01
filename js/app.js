593797c9e22c12e47130f6fe46a382f802a364ca
Jacob_Wendt <jacob.wendt@yahoo.com>
2019-04-01 16:17:43 -0500

    constructed

---
+++ js/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+function Job(companyName, annualSalary, sponsored, location, summarry, link) {
+    this.companyName = companyName,
+    this.annualSalary = annualSalary,
+    this.sponsored = sponsored,
+    this.location = location,
+    this.summarry = summarry,
+    this.link = link;
+};
