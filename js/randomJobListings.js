47d0680448fa975352ed9160cb443e4e454ab4f1
RoryLetteney <rory.letteney@gmail.com>
2019-04-02 10:51:39 -0500

    Finished createJobCard function

--- js/randomJobListings.js
+++ js/randomJobListings.js
@@ -9,7 +9,8 @@ function createEl(tag) {
   return document.createElement(tag);
 }
 
-function createJobCard() {
+function createJobCard(parent, title, name, loc, pay, desc, link) {
+  var parentEl = document.getElementById(parent);
   var div = createEl('div');
   div.className = 'card third shadow';
   var jobTitle = createEl('h1');
@@ -21,11 +22,30 @@ function createJobCard() {
   var compensation = createEl('p');
   compensation.className = 'pay';
   var jobSummary = createEl('p');
-  var link = createEl('a');
+  var jobLink = createEl('a');
   var ad = createEl('i');
   ad.className = 'fas fa-ad';
+
+  jobTitle.innerText = title;
+  companyName.innerText = name;
+  location.appendChild(mapMarker);
+  location.innerText = loc;
+  compensation.innerText = pay;
+  jobSummary.innerText = desc;
+  jobLink.innerText = link;
+
+  parentEl.appendChild(div);
+  div.appendChild(jobTitle);
+  div.appendChild(companyName);
+  div.appendChild(location);
+  div.appendChild(compensation);
+  div.appendChild(jobSummary);
+  div.appendChild(jobLink);
+  if (parentEl === 'sponsored-cards') {
+    div.appendChild(ad);
+  }
 }
 
 function renderJobCard() {
-
+  
 }
