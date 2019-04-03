d9ec3ec7def6b670143f557022230f9ec32bd2c0
RoryLetteney <rory.letteney@gmail.com>
2019-04-03 15:32:43 -0500

    Fixed savedJobs errors

--- js/savedJobs.js
+++ js/savedJobs.js
@@ -1,27 +1,23 @@
-// var savedjobs = [];
+function addheart() {
+  var heart = document.createElement('i');
+  heart.className = 'fas fa-heart fa-2x';
+  var cards = document.getElementsByClassName('card');
+  var hearts = document.getElementsByClassName('fa-heart');
+
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].appendChild(heart.cloneNode(true));
+    hearts[i].addEventListener('click', savedJobHandler);
+  }
+}
+
+addheart();
 
 
+var savedjobs = [];
 
-// function addheart() {
-//   var heart = document.createElement('i');
-//   heart.className = 'fas fa-heart fa-2x';
-//   heart[i].addEventListener('click', savedJobHandler);
-//   var cards = document.getElementsByClassName('card');
-
-//   console.log(cards);
-//   for (var i = 0; i < cards.length; i++) {
-//     cards[i].appendChild(heart.cloneNode(true));
-//   }
-// }
-
-// addheart();
-
-
-// var savedjobs = [];
-
-// function savedJobHandler(e) {
-//     savedjobs.push();
-// }
+function savedJobHandler(e) {
+  
+}
 
 
 
