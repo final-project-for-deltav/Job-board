f7eb6e2d96a1a3ef14b72a5755e44f1eb869cefe
Michele Saba <michelemsaba@gmail.com>
2019-04-03 13:06:50 -0500

    uncommented handle function in savedJobs.js to continue

--- js/savedJobs.js
+++ js/savedJobs.js
@@ -5,28 +5,23 @@ var savedjobs = [];
 function addheart() {
   var heart = document.createElement('i');
   heart.className = 'fas fa-heart fa-2x';
-//   heart.addEventListener('click', savedJobHandler);
+  hearts[i].addEventListener('click', savedJobHandler);
   var cards = document.getElementsByClassName('card');
 
   console.log(cards);
   for (var i = 0; i < cards.length; i++) {
     cards[i].appendChild(heart.cloneNode(true));
-
   }
 }
 
 addheart();
 
 
+var savedjobs = [];
 
-
-//when the heart is clicked 
-
-// var savedjobs = [];
-
-// savedJobHandler(e) {
-//     savedjobs.push();
-// }
+savedJobHandler(e) {
+    savedjobs.push();
+}
 
 
 
