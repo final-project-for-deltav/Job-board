4ae935ddc881dc6176e88d5ecf6a1457ad06a417
RoryLetteney <rory.letteney@gmail.com>
2019-04-03 14:29:09 -0500

    Moved the backface-visibility stylings to style.css

--- js/populateJobListings.js
+++ js/populateJobListings.js
@@ -24,7 +24,7 @@ function createJobCardFace(parent, title, name, loc, pay) {
   var elements = document.querySelectorAll(`[data-_id='${parent}']`);
   var parentEl = elements[elements.length - 1];
   var cardFace = createEl('div');
-  cardFace.className = 'card shadow';
+  cardFace.className = 'card shadow card-front';
   var jobTitle = createEl('h1');
   var companyName = createEl('h2');
   var locationDiv = createEl('div');
@@ -60,7 +60,7 @@ function createJobCardBack(parent, desc, link) {
   var elements = document.querySelectorAll(`[data-_id='${parent}']`);
   var parentEl = elements[elements.length - 1];
   var cardBack = createEl('div');
-  cardBack.className = 'card shadow';
+  cardBack.className = 'card shadow card-back';
   var jobSummary = createEl('p');
   var jobLink = createEl('a');
   jobLink.setAttribute('href', link);
@@ -76,8 +76,6 @@ function createJobCardBack(parent, desc, link) {
   var cardFront = cardBack.previousSibling;
   var cardFrontHeight = cardFront.clientHeight;
   cardBack.style.height = `${cardFrontHeight}px`;
-  cardBack.style.backfaceVisibility = 'hidden';
-  cardBack.style.WebkitBackfaceVisibility = 'hidden';
   cardBack.style.transform = `translateY(-${parentEl.parentNode.id === 'sponsored-cards' ? (parentEl.parentNode.clientHeight - 15) : (cardFrontHeight + 15)}px) rotateY(180deg)`;
   parentEl.style.height = `${cardFrontHeight}px`;
 }
