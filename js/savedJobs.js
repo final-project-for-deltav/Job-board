1fe83bae2fcecc4e73bf1e7c0aaa4808fbe07ea3
Jacob_Wendt <jacob.wendt@yahoo.com>
2019-04-04 10:32:08 -0500

    add the filter by city function to the index.html page

--- js/savedJobs.js
+++ js/savedJobs.js
@@ -1,53 +1,23 @@
+var savedjobs = [];
+
 function addheart() {
   var heart = document.createElement('i');
-  heart.className = 'fas fa-heart';
-  var hearts = document.getElementsByClassName('fa-heart');
+  heart.className = 'fas fa-heart fa-2x';
+  hearts = document.getElementsByClassName("fa-heart")
+  hearts[i].addEventListener('click', savedJobHandler);
   var cardBacks = document.getElementsByClassName('card-back');
-  var hearts = document.getElementsByClassName('fa-heart');
-
-  //   console.log(cards);
-  for (var i = 0; i < cardBacks.length; i++) {
-    cardBacks[i].appendChild(heart.cloneNode(true));
-    hearts[i].addEventListener('click', savedJobHandler);
+  console.log(cards);
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].appendChild(heart.cloneNode(true));
   }
-}
+} 
 
 addheart();
 
-
-var savedJobs = JSON.parse(localStorage.getItem('savedJobs')) || [];
+var savedjobs = [];
 
 function savedJobHandler(e) {
-  // var x = document.getElementById("i").parentNode.nodeName;
-  var index = e.target.parentNode.parentNode.getAttribute('data-_id');
-  if (!savedJobs.includes(Job.all[index])) {
-    savedJobs.push(Job.all[index]);
-    localStorage.setItem('savedJobs', JSON.stringify(savedJobs));
-  }
-  console.log(savedJobs);
-
-
-}
-function renderSavedJobs() {
-  var jobsDiv = document.getElementById('saved-jobs');
-  for (var i = 0; i < savedJobs.length; i++) {
-    console.log(savedJobs);
-    var jobTitle = document.createElement('h1');
-    var companyName = document.createElement('h2');
-    var jobLink = document.createElement('a');
-    var ellink = savedJobs[i].link;
-    jobLink.setAttribute('href', ellink);
-    jobLink.setAttribute('target', 'blank');
-    jobLink.innerText = 'Apply Here';
-    jobTitle.innerText = savedJobs[i].jobTitle;
-    companyName.innerText = savedJobs[i].companyName;
-    jobsDiv.appendChild(jobTitle);
-    jobsDiv.appendChild(companyName);
-    jobsDiv.appendChild(jobLink);
-  }
-}
-renderSavedJobs();
-
-
-
-
+  var kippy = e.target.parentNode
+  var index = parseInt(kippy.getAttribute('data-_id'))
+  Job.all[index]
+};
