3a33a0a9b631adcf534618f4cf8a84cdf7f519c5
RoryLetteney <rory.letteney@gmail.com>
2019-04-04 12:08:39 -0500

    Added rendering of the saved class based upon savedJobs in localStorage on page load.

--- js/savedJobs.js
+++ js/savedJobs.js
@@ -1,4 +1,4 @@
-var savedJobs = [];
+var savedJobs = localStorage['savedJobs'] ? JSON.parse(localStorage['savedJobs']) : [];
 
 function addheart() {
   var heart = document.createElement('i');
@@ -20,6 +20,20 @@ function addheart() {
 }
 
 addheart();
+
+function checkSavedJobs() {
+  var hearts = Object.values(document.getElementsByClassName('fa-heart'));
+  hearts.forEach(function(heart) {
+    var index = parseInt(heart.parentNode.parentNode.getAttribute('data-_id'));
+    for (var job of savedJobs) {
+      if (job._id === index) {
+        heart.classList.add('saved');
+      }
+    }
+  });
+}
+
+checkSavedJobs();
 
 function savedJobHandler(e) {
   if (e.target.classList.contains('saved')) {
@@ -44,42 +58,44 @@ function savedJobHandler(e) {
 
 function renderSavedJobs() {
   var jobsDiv = document.getElementById('saved-jobs');
-  for (var i = 0; i < savedJobs.length; i++) {
-    var card = document.createElement('div');
-    card.className = 'card';
-    var cardContentsLeft = document.createElement('div');
-    cardContentsLeft.className = 'cardContentsLeft';
-    var cardContentsRight = document.createElement('div');
-    cardContentsRight.className = 'cardContentsRight';
-    var cardContentsFull = document.createElement('div');
-    cardContentsFull.className = 'cardContentsFull';
+  if (jobsDiv) {
+    for (var i = 0; i < savedJobs.length; i++) {
+      var card = document.createElement('div');
+      card.className = 'card';
+      var cardContentsLeft = document.createElement('div');
+      cardContentsLeft.className = 'cardContentsLeft';
+      var cardContentsRight = document.createElement('div');
+      cardContentsRight.className = 'cardContentsRight';
+      var cardContentsFull = document.createElement('div');
+      cardContentsFull.className = 'cardContentsFull';
 
-    var jobTitle = document.createElement('h1');
-    var location = document.createElement('p');
-    var companyName = document.createElement('h2');
-    var jobLink = document.createElement('a');
-    var ellink = savedJobs[i].link;
-    var linkIcon = document.createElement('i');
-    var jobSummary  = document.createElement('p');
-    linkIcon.className = 'fas fa-external-link-alt icon';
-    jobLink.setAttribute('href', ellink);
-    jobLink.setAttribute('target', 'blank');
-    jobLink.innerText= 'Apply at Employers Site';
-    jobTitle.innerText = savedJobs[i].jobTitle;
-    location.innerText = savedJobs[i].location + ', IA';
+      var jobTitle = document.createElement('h1');
+      var location = document.createElement('p');
+      var companyName = document.createElement('h2');
+      var jobLink = document.createElement('a');
+      var ellink = savedJobs[i].link;
+      var linkIcon = document.createElement('i');
+      var jobSummary  = document.createElement('p');
+      linkIcon.className = 'fas fa-external-link-alt icon';
+      jobLink.setAttribute('href', ellink);
+      jobLink.setAttribute('target', 'blank');
+      jobLink.innerText= 'Apply at Employers Site';
+      jobTitle.innerText = savedJobs[i].jobTitle;
+      location.innerText = savedJobs[i].location + ', IA';
 
-    companyName.innerText = savedJobs[i].companyName;
-    jobSummary.innerText = savedJobs[i].jobSummary;
-    jobsDiv.appendChild(card);
-    card.appendChild(cardContentsLeft);
-    card.appendChild(cardContentsRight);
-    card.appendChild(cardContentsFull);
-    cardContentsLeft.appendChild(jobTitle);
-    cardContentsLeft.appendChild(location);
-    cardContentsLeft.appendChild(companyName);
-    cardContentsRight.appendChild(jobLink);
-    cardContentsFull.appendChild(jobSummary);
-    jobLink.appendChild(linkIcon);
+      companyName.innerText = savedJobs[i].companyName;
+      jobSummary.innerText = savedJobs[i].jobSummary;
+      jobsDiv.appendChild(card);
+      card.appendChild(cardContentsLeft);
+      card.appendChild(cardContentsRight);
+      card.appendChild(cardContentsFull);
+      cardContentsLeft.appendChild(jobTitle);
+      cardContentsLeft.appendChild(location);
+      cardContentsLeft.appendChild(companyName);
+      cardContentsRight.appendChild(jobLink);
+      cardContentsFull.appendChild(jobSummary);
+      jobLink.appendChild(linkIcon);
+    }
   }
 }
 renderSavedJobs();
